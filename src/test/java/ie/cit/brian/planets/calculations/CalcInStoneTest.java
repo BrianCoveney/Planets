536cefef909b610a66514636dc043f40acfe204c
BrianCoveney <coveneybrian@gmail.com>
2017-04-23 16:47:05 +0100

    added assertion to sorting test

--- src/test/java/ie/cit/brian/planets/calculations/CalcInStoneTest.java
+++ src/test/java/ie/cit/brian/planets/calculations/CalcInStoneTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 
 import java.util.*;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * Created by brian on 23/04/17.
  */
@@ -36,11 +39,25 @@ public class CalcInStoneTest {
         list = SortMap.sortMapByValue(map);
 
         String output;
+
         List<String> arrayList = new ArrayList<>();
+        int current = 0;
+        int previous = 0;
         for(Map.Entry<String, Integer> entry : list) {
             output = entry.getKey() + " " + entry.getValue();
+
+            current = entry.getValue();
+
             arrayList.add(output);
+
         }
+
+        previous = current;
+
+        if(previous > 0) {
+            assertThat(current >= previous, is(true));
+        }
+
 
         screenWriter.showResult(arrayList.toString());
     }
